5b3d8e8669295eec656b3a58d371ed4a78472414
SadiaAnjumTumpa <92468396+SadiaAnjumTumpa@users.noreply.github.com>
2024-07-24 13:59:24 -0400

    Create submodule.py
    Helper function for models

---
+++ submodule.py
@@ -0,0 +1,429 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+import numpy as np
+import math
+from torch.nn import init
+
+class PatchEmbed(nn.Module):
+  def __init__(self, img_size, patch_size, in_chans=3, embed_dim=768):
+        super().__init__()
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.n_patches = int((img_size[0] * img_size[1]) / (patch_size ** 2))
+
+        self.proj = nn.Conv2d(
+                in_chans,
+                embed_dim,
+                kernel_size=patch_size,
+                stride=patch_size,
+        )
+
+  def forward(self, x):
+        """Run forward pass.
+        Parameters
+        ----------
+        x : torch.Tensor
+            Shape `(n_samples, in_chans, img_size, img_size)`.
+        Returns
+        -------
+        torch.Tensor
+            Shape `(n_samples, n_patches, embed_dim)`.
+        """
+        x = self.proj(x)  # (n_samples, embed_dim, img_size[0]/patch_size, img_size[0]/patch_size)
+        #print("after proj patch embed",x.size())
+        x = x.flatten(2)  # (n_samples, embed_dim, n_patches)
+        #print("after flatten", x.size())
+        x = x.transpose(1, 2)  # (n_samples, n_patches, embed_dim)
+        #print("after transpose",x.size())
+        return x
+
+class PatchEmbedEvents(nn.Module):
+  def __init__(self, img_size, patch_size, in_chans=3, embed_dim=768):
+        super().__init__()
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.n_patches = int((img_size[0] * img_size[1]) / (patch_size ** 2))
+        self.embed_dim = 768
+        self.proj = nn.Conv2d(
+                in_chans,
+                embed_dim,
+                kernel_size=patch_size,
+                stride=patch_size,
+        )
+
+  def forward(self, x):
+        """Run forward pass.
+        Parameters
+        ----------
+        x : torch.Tensor
+            Shape `(n_samples, in_chans, img_size, img_size)`.
+        Returns
+        -------
+        torch.Tensor
+            Shape `(n_samples, n_patches, embed_dim)`.
+        """
+        seq_len = x.shape[1]
+        concat=[]#torch.zeros(x.shape[0], n_patches, embed_dim).astype(np.float32)
+        #print(concat.size())
+        for t in range(seq_len):
+            x_t = x[:,t, :, :].unsqueeze(1)
+            #print(x_t.size())
+            x_t = self.proj(x_t)  # (n_samples, embed_dim, img_size[0]/patch_size, img_size[0]/patch_size)
+            #print("after proj patch embed",x_t.size())
+            x_t = x_t.flatten(2)  # (n_samples, embed_dim, n_patches)
+            #print("after flatten", x.size())
+            x_t = x_t.transpose(1, 2)  # (n_samples, n_patches, embed_dim)
+            concat.append(x_t)
+            #print("after transpose",x_t.size())
+        result = torch.zeros(x.shape[0],5*self.n_patches, self.embed_dim)
+        for i, tensor in enumerate(concat):
+          result[:, i*196: (i+1)*196, :] = tensor
+        #print("concat",result.size())
+        #exit()
+        return result.to(x.device)
+        
+class Attention(nn.Module):
+    """Attention mechanism.
+    Parameters
+    ----------
+    dim : int
+        The input and out dimension of per token features.
+    n_heads : int
+        Number of attention heads.
+    qkv_bias : bool
+        If True then we include bias to the query, key and value projections.
+    attn_p : float
+        Dropout probability applied to the query, key and value tensors.
+    proj_p : float
+        Dropout probability applied to the output tensor.
+    Attributes
+    ----------
+    scale : float
+        Normalizing consant for the dot product.
+    qkv : nn.Linear
+        Linear projection for the query, key and value.
+    proj : nn.Linear
+        Linear mapping that takes in the concatenated output of all attention
+        heads and maps it into a new space.
+    attn_drop, proj_drop : nn.Dropout
+        Dropout layers.
+    """
+    def __init__(self, dim, n_heads=12, qkv_bias=True, attn_p=0., proj_p=0.):
+        super().__init__()
+        self.n_heads = n_heads
+        self.dim = dim
+        self.head_dim = dim // n_heads
+        self.scale = self.head_dim ** -0.5
+
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_p)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_p)
+
+    def forward(self, x):
+        """Run forward pass.
+        Parameters
+        ----------
+        x : torch.Tensor
+            Shape `(n_samples, n_patches + 1, dim)`.
+        Returns
+        -------
+        torch.Tensor
+            Shape `(n_samples, n_patches + 1, dim)`.
+        """
+        n_samples, n_tokens, dim = x.shape
+
+        if dim != self.dim:
+            raise ValueError
+
+        qkv = self.qkv(x)  # (n_samples, n_patches + 1, 3 * dim)
+        #print("qkv",qkv.size())
+        #print(n_samples, n_tokens, dim, self.n_heads,self.head_dim)
+        qkv = qkv.reshape(
+                n_samples, n_tokens, 3, self.n_heads, self.head_dim
+        )  # (n_smaples, n_patches + 1, 3, n_heads, head_dim)
+        qkv = qkv.permute(
+                2, 0, 3, 1, 4
+        )  # (3, n_samples, n_heads, n_patches + 1, head_dim)
+        
+        q, k, v = qkv[0], qkv[1], qkv[2]
+        k_t = k.transpose(-2, -1)  # (n_samples, n_heads, head_dim, n_patches + 1)
+        dp = (
+           q @ k_t
+        ) * self.scale # (n_samples, n_heads, n_patches + 1, n_patches + 1)
+        attn = dp.softmax(dim=-1)  # (n_samples, n_heads, n_patches + 1, n_patches + 1)
+        attn = self.attn_drop(attn)
+
+        weighted_avg = attn @ v  # (n_samples, n_heads, n_patches +1, head_dim)
+        weighted_avg = weighted_avg.transpose(
+                1, 2
+        )  # (n_samples, n_patches + 1, n_heads, head_dim)
+        weighted_avg = weighted_avg.flatten(2)  # (n_samples, n_patches + 1, dim)
+
+        x = self.proj(weighted_avg)  # (n_samples, n_patches + 1, dim)
+        x = self.proj_drop(x)  # (n_samples, n_patches + 1, dim)
+
+        return x
+
+class MLP(nn.Module):
+    """Multilayer perceptron.
+    Parameters
+    ----------
+    in_features : int
+        Number of input features.
+    hidden_features : int
+        Number of nodes in the hidden layer.
+    out_features : int
+        Number of output features.
+    p : float
+        Dropout probability.
+    Attributes
+    ----------
+    fc : nn.Linear
+        The First linear layer.
+    act : nn.GELU
+        GELU activation function.
+    fc2 : nn.Linear
+        The second linear layer.
+    drop : nn.Dropout
+        Dropout layer.
+    """
+    def __init__(self, in_features, hidden_features, out_features, p=0.):
+        super().__init__()
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = nn.GELU()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(p)
+
+    def forward(self, x):
+        """Run forward pass.
+        Parameters
+        ----------
+        x : torch.Tensor
+            Shape `(n_samples, n_patches + 1, in_features)`.
+        Returns
+        -------
+        torch.Tensor
+            Shape `(n_samples, n_patches +1, out_features)`
+        """
+        x = self.fc1(
+                x
+        ) # (n_samples, n_patches + 1, hidden_features)
+        x = self.act(x)  # (n_samples, n_patches + 1, hidden_features)
+        x = self.drop(x)  # (n_samples, n_patches + 1, hidden_features)
+        x = self.fc2(x)  # (n_samples, n_patches + 1, out_features)
+        x = self.drop(x)  # (n_samples, n_patches + 1, out_features)
+
+        return x
+
+class Dec_Block(nn.Module):
+    """Transformer block.
+    Parameters
+    ----------
+    dim : int
+        Embeddinig dimension.
+    n_heads : int
+        Number of attention heads.
+    mlp_ratio : float
+        Determines the hidden dimension size of the `MLP` module with respect
+        to `dim`.
+    qkv_bias : bool
+        If True then we include bias to the query, key and value projections.
+    p, attn_p : float
+        Dropout probability.
+    Attributes
+    ----------
+    norm1, norm2 : LayerNorm
+        Layer normalization.
+    attn : Attention
+        Attention module.
+    mlp : MLP
+        MLP module.
+    """
+    def __init__(self, dim, n_heads, mlp_ratio=4.0, qkv_bias=True, p=0., attn_p=0.):
+        super().__init__()
+        self.norm1 = nn.LayerNorm(dim, eps=1e-6)
+        self.attn = Attention(
+                dim,
+                n_heads=n_heads,
+                qkv_bias=qkv_bias,
+                attn_p=attn_p,
+                proj_p=p
+        )
+        self.cross_attn = Cross_Attention(
+                dim,
+                n_heads=n_heads,
+                qkv_bias=qkv_bias,
+                attn_p=attn_p,
+                proj_p=p
+        )
+        self.norm2 = nn.LayerNorm(dim, eps=1e-6)
+        hidden_features = int(dim * mlp_ratio)
+        self.mlp = MLP(
+                in_features=dim,
+                hidden_features=hidden_features,
+                out_features=dim,
+        )
+
+    def forward(self, y, x):
+        """Run forward pass.
+        Parameters
+        ----------
+        x : torch.Tensor
+            Shape `(n_samples, n_patches + 1, dim)`.
+        Returns
+        -------
+        torch.Tensor
+            Shape `(n_samples, n_patches + 1, dim)`.
+        """
+        y = y + self.attn(self.norm1(y))
+        y=self.norm1(y)
+        y = y + self.cross_attn(y,x)
+        y = y + self.mlp(self.norm2(y))
+
+        return y
+                
+class Enc_Block(nn.Module):
+    """Transformer block.
+    Parameters
+    ----------
+    dim : int
+        Embeddinig dimension.
+    n_heads : int
+        Number of attention heads.
+    mlp_ratio : float
+        Determines the hidden dimension size of the `MLP` module with respect
+        to `dim`.
+    qkv_bias : bool
+        If True then we include bias to the query, key and value projections.
+    p, attn_p : float
+        Dropout probability.
+    Attributes
+    ----------
+    norm1, norm2 : LayerNorm
+        Layer normalization.
+    attn : Attention
+        Attention module.
+    mlp : MLP
+        MLP module.
+    """
+    def __init__(self, dim, n_heads, mlp_ratio=4.0, qkv_bias=True, p=0., attn_p=0.):
+        super().__init__()
+        self.norm1 = nn.LayerNorm(dim, eps=1e-6)
+        self.attn = Attention(
+                dim,
+                n_heads=n_heads,
+                qkv_bias=qkv_bias,
+                attn_p=attn_p,
+                proj_p=p
+        )
+        self.norm2 = nn.LayerNorm(dim, eps=1e-6)
+        hidden_features = int(dim * mlp_ratio)
+        self.mlp = MLP(
+                in_features=dim,
+                hidden_features=hidden_features,
+                out_features=dim,
+        )
+
+    def forward(self, x):
+        """Run forward pass.
+        Parameters
+        ----------
+        x : torch.Tensor
+            Shape `(n_samples, n_patches + 1, dim)`.
+        Returns
+        -------
+        torch.Tensor
+            Shape `(n_samples, n_patches + 1, dim)`.
+        """
+        x = x + self.attn(self.norm1(x))
+        x = x + self.mlp(self.norm2(x))
+
+        return x
+
+ 
+class DenseResidualBlock(nn.Module):
+    """
+    The Dense Residual Block
+    """
+
+    def __init__(self, filters, res_scale=0.2):
+        super(DenseResidualBlock, self).__init__()
+        self.res_scale = res_scale
+
+        def block(in_features, non_linearity=True):
+            layers = [nn.Conv2d(in_features, filters, 3, 1, 1, bias=True)]
+            if non_linearity:
+                layers += [nn.LeakyReLU()]
+            return nn.Sequential(*layers)
+
+        self.b1 = block(in_features=1 * filters)
+        self.b2 = block(in_features=2 * filters)
+        self.b3 = block(in_features=3 * filters)
+        self.b4 = block(in_features=4 * filters)
+        self.b5 = block(in_features=5 * filters, non_linearity=False)
+    
+    def forward(self, x):
+        inputs = x
+        
+        out = self.b1(inputs)
+        inputs = torch.cat([inputs, out], 1)
+        
+        out = self.b2(inputs)
+        inputs = torch.cat([inputs, out], 1)
+        
+        out = self.b3(inputs)
+        inputs = torch.cat([inputs, out], 1)
+        
+        out = self.b4(inputs)
+        inputs = torch.cat([inputs, out], 1)
+        
+        out = self.b5(inputs)
+        
+        
+        return out.mul(self.res_scale) + x
+
+
+class ResidualInResidualDenseBlock(nn.Module):
+    def __init__(self, filters, res_scale=0.2):
+        super(ResidualInResidualDenseBlock, self).__init__()
+        self.res_scale = res_scale
+        self.dense_blocks = nn.Sequential(
+            DenseResidualBlock(filters), DenseResidualBlock(filters), DenseResidualBlock(filters))
+
+    def forward(self, x):
+        return self.dense_blocks(x).mul(self.res_scale) + x
+
+class ConvLayer(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, activation='relu', norm=None,
+                 BN_momentum=0.1):
+        super(ConvLayer, self).__init__()
+
+        bias = False if norm == 'BN' else True
+        self.conv2d = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=bias)
+        if activation is not None:
+            self.activation = getattr(torch, activation)
+        else:
+            self.activation = None
+
+        self.norm = norm
+        if norm == 'BN':
+            self.norm_layer = nn.BatchNorm2d(out_channels, momentum=BN_momentum)
+        elif norm == 'IN':
+            self.norm_layer = nn.InstanceNorm2d(out_channels, track_running_stats=True)
+
+    def forward(self, x):
+        out = self.conv2d(x)
+
+        if self.norm in ['BN', 'IN']:
+            out = self.norm_layer(out)
+
+        if self.activation is not None:
+            out = self.activation(out)
+
+        return out
+                
+
+
+ 
